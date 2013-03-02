6fce53948ece829d0aa1f8074e51729254121e10
Deepak Jagannath <deciblast@gmail.com>
2013-03-02 15:59:32 -0800

    Get online users
    Initial check in

---
+++ getusersonline.php
@@ -0,0 +1,33 @@
+<?php header('content-type: application/json; charset=utf-8');
+
+	$mysql_user = 'test';
+	$mysql_password = 'testing123';
+
+	$callback = '';
+	
+	if (isset($_GET['callback']))
+	{
+		$callback = filter_var($_GET['callback'], FILTER_SANITIZE_STRING);
+	}
+
+	$link = mysql_connect('localhost', $mysql_user, $mysql_password);
+	
+	if (!$link) {
+   	 	die('Could not connect: ' . mysql_error());
+	}
+
+	$result = mysql_query('SELECT id, username WHERE isOnline = 1');
+	
+	if (!$result) {
+    	die('Invalid query: ' . mysql_error());
+	}
+
+	while($r = mysql_fetch_assoc($result)) {
+		 $data[] = $r;
+   	}
+	
+	mysql_close($link);
+
+	echo $callback . '('.json_encode($data).')';
+
+?>
