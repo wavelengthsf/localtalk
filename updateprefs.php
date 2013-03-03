fea0cefea5d559c0f6cd6e1fc97528e5a431e873
unknown <wavelength@gmail.com>
2013-03-02 16:39:32 -0800

    test

---
+++ updateprefs.php
@@ -0,0 +1,42 @@
+<?php 
+/*update prefs saves the user's interests */
+	$callback = '';
+/*	if (isset($_GET['callback']))
+	{ */
+	//	$callback = filter_var($_GET['callback'], FILTER_SANITIZE_STRING);
+		$userid = filter_var($_GET['userid'], FILTER_SANITIZE_STRING);
+		$topic = filter_var($_GET['topic'], FILTER_SANITIZE_STRING);
+/*	} */
+echo $userid;
+	echo "<br>";
+	echo $topic;
+
+/*write user prefs to db
+10.100.26.245:3306
+db: localtalk
+u: test
+p: testing123
+
+*/
+$con=mysqli_connect("10.100.26.245","test","testing123","localtalk");
+
+//Writes the information to the database
+mysql_query("INSERT INTO `interestname` (`interestname`) VALUES ('$topic')") ;
+
+$dynaqry = "select interestid from interestname where interestname = '".$topic."'";
+echo "<br>".$dynaqry;
+
+//get the ID that the database just generated
+$topicid = mysqli_query($con,$dynaqry);
+
+while($row = mysqli_fetch_array($topicid))
+  {
+  echo "<br> ID:".$row['interestid'];
+  echo "<br />";
+  }
+ 
+mysqli_close($con);
+
+//mysql_query("select interestid from interestname = '$topic'");
+
+?>
